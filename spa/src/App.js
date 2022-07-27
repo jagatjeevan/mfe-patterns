11fb63596da4f20698d09948c357716f76ab4fca
Jagat Jeevan Sahoo <jagatsah@thoughtworks.com>
2022-07-27 15:23:52 +0530

    update route paths

--- spa/src/App.js
+++ spa/src/App.js
@@ -1,14 +1,23 @@
 import * as React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Cart from "./components/cart";
+import Dashboard from "./components/dashboard";
 import Footer from "./components/footer";
 import Header from "./components/header";
+import ProductDetails from "./components/productDetails";
 import { StyledAppContent, AppLayout } from './styles/layout';
 
 const App = () => (
   <AppLayout className="App">
     <Header />
-    <StyledAppContent><Cart /></StyledAppContent>
+    <StyledAppContent>
+      <Cart />
+      {/* <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/products/:productId" element={<ProductDetails />} />
+        </Routes> */}
+    </StyledAppContent>
     <Footer />
   </AppLayout>
 );;
